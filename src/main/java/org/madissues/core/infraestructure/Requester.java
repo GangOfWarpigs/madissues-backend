88898f73fb7317260766ad8755389634689a8875
josericardopenase <josericardopenase@gmail.com>
2024-02-06 21:36:45 +0100

    Uncomment requester

--- src/main/java/org/madissues/core/infraestructure/Requester.java
+++ src/main/java/org/madissues/core/infraestructure/Requester.java
@@ -1,6 +1,7 @@
 package org.madissues.core.infraestructure;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import java.io.IOException;
 import java.net.URI;
@@ -11,13 +12,13 @@ import java.net.http.HttpResponse;
 
 public class Requester {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
-       //HttpRequest request = HttpRequest.newBuilder()
-       //        .GET()
-       //        .uri(new URI("https://catfact.ninja/fact"))
-       //        .build();
+       HttpRequest request = HttpRequest.newBuilder()
+               .GET()
+               .uri(new URI("https://catfact.ninja/fact"))
+               .build();
 
-       //var joke = getJsonObject(request, Joke.class);
-       //System.out.println(joke.fact);
+       var joke = getJsonObject(request, JsonObject.class);
+       System.out.println(joke.get("fact"));
     }
 
     private static <T> T getJsonObject(HttpRequest request, Class<T> classObject) throws IOException, InterruptedException {
