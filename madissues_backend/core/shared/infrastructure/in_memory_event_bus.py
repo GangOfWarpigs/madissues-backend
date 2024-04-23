5a8bdb26607064e0419616e7c276221c19d09601
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-04-23 11:44:31 +0100

    Implemented InMemoryEventBus (mock)

---
+++ madissues_backend/core/shared/infrastructure/in_memory_event_bus.py
@@ -0,0 +1,19 @@
+from madissues_backend.core.shared.application.event_bus import EventBus
+from madissues_backend.core.shared.application.event_handler import EventHandler
+from madissues_backend.core.shared.domain.events import DomainEvent
+
+
+class InMemoryEventBus(EventBus):
+
+    handlers: dict[str, list[EventHandler]] = {}
+
+    def subscribe(self, handler: EventHandler):
+        self.handlers.setdefault(handler.event_name, []).append(handler)
+
+    def notify(self, event: DomainEvent):
+        for handler in self.handlers.get(event.name, []):
+            handler.handle(event)
+
+    def notify_all(self, events: list[DomainEvent]):
+        for event in events:
+            self.notify(event)
