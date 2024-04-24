063786368fa1f7c569c0f8c165e25fbbdbafdb82
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-04-25 00:41:33 +0100

    Implemented Issue value-objects with tests

--- madissues_backend/core/issues/domain/issue.py
+++ madissues_backend/core/issues/domain/issue.py
@@ -1,16 +1,23 @@
 from datetime import datetime
+from typing import Annotated
+
+from pydantic import Field
 
 from madissues_backend.core.organizations.domain.organization import LinkToImage, Description
 from madissues_backend.core.shared.domain.entity import AggregateRoot
 from madissues_backend.core.shared.domain.value_objects import GenericUUID
 
+Title = Annotated[str, Field(min_length=2, max_length=80)]
+Details = Annotated[str, Field(min_length=2, max_length=1000)]
+Status = Annotated[str, Field(min_length=1, max_length=20, pattern=r'^(Queued|In progress|Solved|Not Solved)$')]
+
 
 class Issue(AggregateRoot[GenericUUID]):
-    title: str
+    title: Title
     description: Description
-    details: str
+    details: Details
     proofs: list[LinkToImage]  # List of image links
-    status: str  # Queued, In progress, Solved, Not Solved
+    status: Status  # Queued, In progress, Solved, Not Solved
     timestamp: datetime
     course: GenericUUID
     teachers: list[GenericUUID]
