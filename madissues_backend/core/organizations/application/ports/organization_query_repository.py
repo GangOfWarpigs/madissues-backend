904ebf93e12d02a7bcca24e31ff5f70dbaebbf2d
josericardopenase <josericardopenase@gmail.com>
2024-05-10 12:33:10 +0100

    Finished read models creation of organization

--- madissues_backend/core/organizations/application/ports/organization_query_repository.py
+++ madissues_backend/core/organizations/application/ports/organization_query_repository.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 
+from madissues_backend.core.organizations.domain.read_models.OrganizationReadModel import OrganizationReadModel
+
 
 class OrganizationQueryRepository(ABC):
     @abstractmethod
-    def get_all_by_owner(self, owner_id: str):
+    def get_all_by_owner(self, owner_id: str) -> list[OrganizationReadModel]:
         ...
     @abstractmethod
-    def get_by_id(self, id: str):
+    def get_by_id(self, id: str) -> OrganizationReadModel:
         ...
---
+++ madissues_backend/core/organizations/domain/read_models/OrganizationReadModel.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from pydantic import Field, BaseModel
+
+from madissues_backend.core.shared.domain.value_objects import GenericUUID, LinkToImage
+
+class OrganizationCourseReadModel(BaseModel):
+    name: str
+    code: str
+    icon: str
+    primary_color: str
+    secondary_color: str
