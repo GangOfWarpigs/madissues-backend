a8839f0861999ef490bf1a7ca5f2e1c521d0be39
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-05-11 03:37:36 +0100

    Fixed Task Manager tests

--- madissues_backend/core/task_manager/tests/domain/test_task_manager.py
+++ madissues_backend/core/task_manager/tests/domain/test_task_manager.py
@@ -11,77 +11,70 @@ class TestTaskManager(unittest.TestCase):
     def setUp(self):
         self.task_manager_factory = Mock(spec=TaskManagerFactory)
         self.task_manager_service = Mock(spec=TaskManagerService)
-
         self.task_manager_factory.of.return_value = self.task_manager_service
 
+        # Estableciendo valores de retorno de los métodos mockeados
         self.task_manager_service.create_empty_board.return_value = "123e4567-e89b-12d3-a456-426614174000"
         self.task_manager_service.create_empty_list.return_value = "list_id"
 
+        # Configuración inicial del TaskManager
         self.task_manager = TaskManager(
             id=GenericUUID.next_id(),
             organization_id=GenericUUID.next_id(),
-            config=TaskManagerConfig(service="trello",
-                                     api_key="test_key",
-                                     api_token="test_token"),
+            config=TaskManagerConfig(service="trello", api_key="test_key", api_token="test_token"),
             task_manager_project_id="123e4567-e89b-12d3-a456-426614174000"
-
         )
 
     def test_generate_infrastructure(self):
         self.task_manager.generate_infrastructure(self.task_manager_factory)
         self.task_manager_factory.of.assert_called_once_with(self.task_manager.config)
-        self.task_manager_service.create_empty_board.assert_any_call("Faqs", self.task_manager_service)
-        self.task_manager_service.create_empty_board.assert_any_call("Issues", self.task_manager_service)
+        self.task_manager_service.create_empty_board.assert_any_call(self.task_manager.task_manager_project_id, "Faqs")
+        self.task_manager_service.create_empty_board.assert_any_call(self.task_manager.task_manager_project_id, "Issues")
         self.assertEqual(self.task_manager_service.create_empty_list.call_count, 8)
         self.assertIsNotNone(self.task_manager.faqs_board)
         self.assertIsNotNone(self.task_manager.issue_board)
 
     def test_generate_idle_board(self):
         board = self.task_manager.generate_idle_board("test", self.task_manager_service)
-        self.task_manager_service.create_empty_board.assert_called_once_with("test")
+        self.task_manager_service.create_empty_board.assert_called_once_with(self.task_manager.task_manager_project_id, "test")
         self.assertEqual(self.task_manager_service.create_empty_list.call_count, 4)
         self.assertIsNotNone(board)
 
     def test_generate_idle_board_with_different_name(self):
         board = self.task_manager.generate_idle_board("different_test", self.task_manager_service)
-        self.task_manager_service.create_empty_board.assert_called_once_with("different_test")
+        self.task_manager_service.create_empty_board.assert_called_once_with(self.task_manager.task_manager_project_id, "different_test")
         self.assertEqual(self.task_manager_service.create_empty_list.call_count, 4)
         self.assertIsNotNone(board)
 
     def test_generate_infrastructure_with_different_config(self):
-        self.task_manager.config = TaskManagerConfig(service="trello", api_key="different_test_key",
-                                                     api_token="different_test_token")
+        self.task_manager.config = TaskManagerConfig(service="trello", api_key="different_test_key", api_token="different_test_token")
         self.task_manager.generate_infrastructure(self.task_manager_factory)
         self.task_manager_factory.of.assert_called_once_with(self.task_manager.config)
-        self.task_manager_service.create_empty_board.assert_any_call("faqs")
-        self.task_manager_service.create_empty_board.assert_any_call("issues")
+        self.task_manager_service.create_empty_board.assert_any_call(self.task_manager.task_manager_project_id, "Faqs")
+        self.task_manager_service.create_empty_board.assert_any_call(self.task_manager.task_manager_project_id, "Issues")
         self.assertEqual(self.task_manager_service.create_empty_list.call_count, 8)
         self.assertIsNotNone(self.task_manager.faqs_board)
         self.assertIsNotNone(self.task_manager.issue_board)
 
-    def test_generate_idle_board_with_different_service(self):
-        self.mock_service2 = Mock(spec=TaskManagerService)
-        self.mock_service2.create_empty_board.return_value = "123e4567-e89b-12d3-a456-426614174000"
-        self.mock_service2.create_empty_list.return_value = "list_id2"
-        board = self.task_manager.generate_idle_board("test", self.mock_service2)
-
     def test_generate_infrastructure_with_same_config(self):
+        # Se llama dos veces a generate_infrastructure para probar con la misma configuración
         self.task_manager.generate_infrastructure(self.task_manager_factory)
         self.task_manager.generate_infrastructure(self.task_manager_factory)
         self.task_manager_factory.of.assert_called_with(self.task_manager.config)
-        self.task_manager_service.create_empty_board.assert_any_call("faqs")
-        self.task_manager_service.create_empty_board.assert_any_call("issues")
-        self.assertEqual(self.task_manager_service.create_empty_list.call_count, 16)
+        self.task_manager_service.create_empty_board.assert_any_call(self.task_manager.task_manager_project_id, "Faqs")
+        self.task_manager_service.create_empty_board.assert_any_call(self.task_manager.task_manager_project_id, "Issues")
+        self.assertEqual(self.task_manager_service.create_empty_list.call_count, 16)  # 8 llamadas por cada generate_infrastructure
         self.assertIsNotNone(self.task_manager.faqs_board)
         self.assertIsNotNone(self.task_manager.issue_board)
 
     def test_generate_infrastructure_with_same_service(self):
+        # Se llama dos veces a generate_infrastructure para probar con la misma configuración
         self.task_manager.generate_infrastructure(self.task_manager_factory)
         self.task_manager.generate_infrastructure(self.task_manager_factory)
         self.task_manager_factory.of.assert_called_with(self.task_manager.config)
-        self.task_manager_service.create_empty_board.assert_any_call("faqs")
-        self.task_manager_service.create_empty_board.assert_any_call("issues")
-        self.assertEqual(self.task_manager_service.create_empty_list.call_count, 16)
+        self.task_manager_service.create_empty_board.assert_any_call(self.task_manager.task_manager_project_id, "Faqs")
+        self.task_manager_service.create_empty_board.assert_any_call(self.task_manager.task_manager_project_id, "Issues")
+        self.assertEqual(self.task_manager_service.create_empty_list.call_count, 16)  # 8 llamadas por cada generate_infrastructure
         self.assertIsNotNone(self.task_manager.faqs_board)
         self.assertIsNotNone(self.task_manager.issue_board)
 
