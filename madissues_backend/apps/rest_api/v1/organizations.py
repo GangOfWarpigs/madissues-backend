b196cfa3ee7f8bcb66bcfcc4df22e408a76f497f
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-05-11 15:15:36 +0100

    Implemented toggle_like_issue_comment_command.py with tests

--- madissues_backend/apps/rest_api/v1/organizations.py
+++ madissues_backend/apps/rest_api/v1/organizations.py
@@ -99,6 +99,7 @@ def get_organization_degrees(id: str) -> Response[list[OrganizationDegreeReadMod
     query = GetOrganizationDegreesQuery(organization_query_repository)
     return query.run(id)
 
+
 @router.get("/organization/{id}/issues/", tags=["organizations"])
 def create_issues(token: Annotated[str, Header()], id: str) -> Response[list[IssueReadModel]]:
     authorization = authorization_service(token)
